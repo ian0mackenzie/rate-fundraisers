47a57933979215f2ce6a030746b0d65fcda4f6aa
Ian Mackenzie <ian0mackenzie@gmail.com>
2025-11-29 14:51:58 -0500

    Sentry corrections

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
